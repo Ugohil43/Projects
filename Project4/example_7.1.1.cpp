b196ad6e82fec68a44eaf264deb4a8ff943f88bb
Ilya <ezhoviv@npo-angstrem.ru>
2022-08-01 17:04:38 +0300

    Update example 7.1.1

--- Project4/example_7.1.1.cpp
+++ Project4/example_7.1.1.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    char str[600];
+    char name[100];
+    char addr[200];
+    char work[200];
+
+    cout << "Enter name and press ENTER: ";
+    cin.getline(name, 99);
+    cout << "Enter address and press ENTER: ";
+    cin.getline(addr, 199);
+    cout << "Enter workplace and press ENTER: ";
+    cin.getline(work, 199);
+
+    strncpy(str, "\nMy name is ", 599);
+    strncat(str, name, 599 - strlen(str));
+    strncat(str, ", I live at ", 599 - strlen(str));
+    strncat(str, addr, 599 - strlen(str));
+    strncat(str, ",and I work at ", 599 - strlen(str));
+    strncat(str, work, 599 - strlen(str));
+    strncat(str, ".", 599 - strlen(str));
+
+    cout << str << endl;
+
     return 0;
 }
