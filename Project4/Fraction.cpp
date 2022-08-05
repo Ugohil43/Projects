8fb4f3cffd43508489b0ba4ae3ba0e9a671efab3
Ilya <ezhoviv@npo-angstrem.ru>
2022-08-05 16:35:22 +0300

    Реализованы упражнения главы 13. Дописан и протестирован класс Fraction.

--- Project4/Fraction.cpp
+++ Project4/Fraction.cpp
@@ -9,10 +9,10 @@ class Fraction
         int num, den;
     public:
         //Fraction() { set(0, 1); }
-        Fraction() { num = 0; den = 0; normalize(); }
-        Fraction(int n, int d) { set(n, d); }
-        Fraction(int n) { set(n, 1); }
-        Fraction(Fraction const &src) { set(src.num, src.den); normalize(); }
+        Fraction() {num = 0; den = 0; normalize();}
+        Fraction(int n, int d) {set(n, d);}
+        Fraction(int n) {set(n, 1);}
+        Fraction(Fraction const &src) {set(src.num, src.den); normalize();}
 
         void set(int n, int d)
         {
@@ -21,10 +21,18 @@ class Fraction
 
         int get_num() {return num;}
         int get_den() {return den;}
-        Fraction add(Fraction other);
-        Fraction mult(Fraction other);
-        Fraction sub(Fraction other);
-        Fraction div(Fraction other);
+        Fraction add(const Fraction &other);
+        Fraction mult(const Fraction &other);
+        Fraction sub(const Fraction &other);
+        Fraction div(const Fraction &other);
+        Fraction operator+(const Fraction &other) {return add(other);}
+        Fraction operator*(const Fraction &other) {return mult(other);}
+        Fraction operator-(const Fraction &other) {return sub(other);}
+        Fraction operator/(const Fraction &other) {return div(other);}
+        bool operator>(const Fraction &other);
+        bool operator<(const Fraction &other);
+        bool operator==(const Fraction &other) {return (num == other.num && den == other.den);}
+        friend ostream &operator<<(ostream &os, Fraction &fr) {return (os << fr.num << "/" << fr.den);}
     private:
         void normalize();
         int gcf(int a, int b);
@@ -64,7 +72,7 @@ int main(int argc, char const *argv[])
     fract.set(100,25);
     cout << "100/25          " << fract.get_num() << "/" << fract.get_den() << endl;
     */
-
+   /*
     Fraction fract1, fract2, fract3;
     int a, b, c, d;
     while (1)
@@ -80,9 +88,24 @@ int main(int argc, char const *argv[])
         cout << "Enter denominator of second fract: ";
         cin >> d;
         fract2.set(c,d);
-        fract3 = fract1.div(fract2);
-        cout << fract1.get_num() << "/" << fract1.get_den() <<  " / " << fract2.get_num() << "/" << fract2.get_den() << " = " << fract3.get_num() << "/" << fract3.get_den() << endl;
+        //fract3 = fract1.div(fract2);
+        fract3 = fract1 / fract2;
+        //cout << fract1.get_num() << "/" << fract1.get_den() <<  " / " << fract2.get_num() << "/" << fract2.get_den() << " = " << fract3.get_num() << "/" << fract3.get_den() << endl;
+        cout << fract1 <<  " / " << fract2 << " = " << fract3 << endl;
     }
+    */
+    Fraction f1(1, 2);
+    Fraction f2(1,3);
+
+    if (f1 > f2)
+        cout << "f1 > f2" << endl;
+    else
+        cout << "f1 < f2" << endl;
+
+    if (f1 < f2)
+        cout << "f1 < f2" << endl;
+    else
+        cout << "f1 > f2" << endl;
     return 0;
 }
 
@@ -119,7 +142,7 @@ int Fraction::lcm(int a, int b)
     return (a / gcf(a,b)) * b;
 }
 
-Fraction Fraction::add(Fraction other)
+Fraction Fraction::add(const Fraction &other)
 {
     Fraction fract;
     int lcd = lcm(den, other.den);
@@ -130,7 +153,7 @@ Fraction Fraction::add(Fraction other)
     return fract;  
 }
 
-Fraction Fraction::mult(Fraction other)
+Fraction Fraction::mult(const Fraction &other)
 {
     Fraction fract;
     fract.set(num * other.num, den * other.den);
@@ -138,7 +161,7 @@ Fraction Fraction::mult(Fraction other)
     return fract;
 }
 
-Fraction Fraction::sub(Fraction other)
+Fraction Fraction::sub(const Fraction &other)
 {
     Fraction fract;
     int lcd = lcm(den, other.den);
@@ -149,10 +172,20 @@ Fraction Fraction::sub(Fraction other)
     return fract;
 }
 
-Fraction Fraction::div(Fraction other)
+Fraction Fraction::div(const Fraction &other)
 {
     Fraction fract;
     fract.set(num * other.den, den * other.num);
     fract.normalize();
     return fract;
+}
+
+bool Fraction::operator>(const Fraction &other)
+{
+    return ((num * other.den) > (den * other.num));
+}
+
+bool Fraction::operator<(const Fraction &other)
+{
+    return ((num * other.den) < (den * other.num));
 }
